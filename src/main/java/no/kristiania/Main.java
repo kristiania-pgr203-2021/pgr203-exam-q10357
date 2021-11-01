35beb0230f87b4a98b9226fc9f5cd744bbee8346
q10357 <isch004@student.kristiania.no>
2021-11-01 19:27:06 +0100

    Initial commit

---
+++ src/main/java/no/kristiania/Main.java
@@ -0,0 +1,12 @@
+package no.kristiania;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Tiffany!");
+        printLine();
+    }
+
+    private static void printLine(){
+        System.out.println("I like pizza");
+    }
+}
---
+++ src/test/java/no/kristiania/TestClass.java
@@ -0,0 +1,12 @@
+package no.kristiania;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    @Test
+    void test() {
+        //assertThat( )
+    }
+}
